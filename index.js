6d7c02855a9371db81b701ed7a981d779a5f4975
Michael Byalsky <64349105+michaelbyalsky@users.noreply.github.com>
2020-08-06 00:55:11 +0300

    with all fitures

---
+++ index.js
@@ -0,0 +1,112 @@
+// const axios = require('axios');
+
+//define the task counter
+let taskCounterNumber;
+
+/*The function checks if there is there is tasks in the local storage and loads them*/
+window.onload = () => {
+  if (localStorage.getItem("saveTask") !== null) {
+    document.querySelector(".listContainer").innerHTML = localStorage.getItem(
+      "saveTask"
+    );
+  }
+  if (localStorage.getItem("saveCounter") !== null) {
+    taskCounterNumber = Number(localStorage.getItem("saveCounter"));
+    taskCounter.innerText = taskCounterNumber;
+  } else {
+    taskCounterNumber = 0;
+  }
+};
+
+const taskInput = document.querySelector("#textInput");
+const addButton = document.querySelector("#get");
+const container = document.querySelector(".listContainer");
+const taskCounter = document.querySelector("#counter");
+const searchBar = document.querySelector("#search");
+const deleteAll = document.querySelector("#deleteAll");
+
+document.addEventListener("click", (e) => {
+  console.log(e.target.id);
+  if (e.target.id === "get") {
+    let nameId = textInput.value;
+    searchProduct(nameId);
+  }
+  if (e.target.id === "post") {
+    // let data = textInput.value;
+    postProduct();
+  }
+  if (e.target.id === "put") {
+    // let data = textInput.value;
+    let nameId = textInput.value;
+    putProduct(nameId);
+  }
+  if (e.target.id === "delete") {
+    // let data = textInput.value;
+    let nameId = textInput.value;
+    deleteProduct(nameId);
+  }
+});
+
+const searchProduct = (nameId) => {
+    console.log(nameId)
+  axios
+    .get(`http://localhost:3000/products/${nameId}`)
+    .then((response) => {
+        console.log(response)
+      showProduct(response.data);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      document.getElementById("error").innerHTML = "<p>Product not found</p>";
+      setTimeout(() => {
+        document.getElementById("error").innerHTML = "";
+      }, 3000);
+    });
+};
+
+const showProduct = (product) => {
+    let productWrapper = document.createElement("div");
+    productWrapper.classList.add("list-group");
+    container.appendChild(productWrapper);
+    console.log(product)
+    productWrapper.innerHTML = `<div class="list-group-item">item: ${product.item}</div>`;
+  };  
+
+const product = {
+  "id": 2,
+  "item": "apple",
+};
+const postProduct = () => {
+  axios
+    .post(`http://localhost:3000/products/`, product)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+const putProduct = (nameId) => {
+    axios
+      .put(`http://localhost:3000/products/${nameId}`, product)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const deleteProduct = (nameId) => {
+    axios
+      .delete(`http://localhost:3000/products/${nameId}`, product)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  
+
--- restfulAPI.js
+++ restfulAPI.js
@@ -1,72 +1,68 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 
 const products = [
-    {
-        id: 1,
-        item: 'cheese'
-    },
-    {
-        id: 4,
-        item: 'bread'
-    },
-    {
-        id: 2,
-        item: 'cookies'
-    },
+  {
+    id: 1,
+    item: "cheese",
+  },
+  {
+    id: 4,
+    item: "bread",
+  },
+  {
+    id: 2,
+    item: "cookies",
+  },
 ];
 
-app.get('/products', (req, res) => {
-    res.send(products);
+app.get("/products", (req, res) => {
+  res.send(products);
 });
 
-app.get('/products/:id', (req, res) => {
-    products.forEach(element => {
-        if (element.id == req.params.id) {
-            res.send(element);
-        }
-    });
-    res.send('no matching item found');
-});
-
-app.post('/products', (req, res) => {
-    if (req.body.id) {
-        products.forEach(element => {
-            if (element.id == req.body.id) {
-                res.send('id is already used, please use a different unique id');
-            }
-        });
-        products.push(req.body);
-        res.send('successfully added to the list');
-    } else {
-        res.send('please enter a valid item with a valid id');
+app.get(`/products/:id`, (req, res) => {
+    for (let product of products) {
+      if (product.id === Number(req.params.id)) {
+        res.send(product);
+      }
     }
+  res.send("no matching item found");
 });
 
-app.put('/products/:id', (req, res) => {
-    products.forEach(element => {
-        if (element.id == req.body.id) {
-            res.send('id is already used, please use a different unique id');
-        }
+app.post("/products", (req, res) => {
+  if (req.body.id) {
+    products.forEach((element) => {
+      if (element.id == req.body.id) {
+        res.send("id is already used, please use a different unique id");
+      }
     });
-    products.forEach(element => {
-        if (element.id == req.params.id) {
-            Object.assign(element, req.body);
-            res.send('item successfully updated');
-        }
-    });
-    res.send('no matching item found');
+    products.push(req.body);
+    res.send("successfully added to the list");
+  } else {
+    res.send(`please enter a valid item with a valid id`);
+  }
 });
 
-app.delete('/products/:id', (req, res) => {
-    products.forEach(element => {
-        if (element.id == req.params.id) {
-            products.splice(products.indexOf(element), 1);
-            res.send('item successfully deleted');
-        }
-    });
-    res.send('no matching item found');
+app.put("/products/:id", (req, res) => {
+  
+  products.forEach((element) => {
+    if (element.id == req.params.id) {
+      Object.assign(element, req.body);
+      res.send("item successfully updated");
+    }
+  });
+  res.send("no matching item found");
+});
+
+app.delete("/products/:id", (req, res) => {
+  products.forEach((element) => {
+    if (element.id == req.params.id) {
+      products.splice(products.indexOf(element), 1);
+      res.send("item successfully deleted");
+    }
+  });
+  res.send("no matching item found");
 });
 
 app.listen(3000);
